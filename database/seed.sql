fc33ccbbe73122ffc48f3da8dc754e62d121b35d
miguelgazela <miguel.gazela@gmail.com>
2013-10-26 07:20:35 +0100

    seed

---
+++ database/seed.sql
@@ -0,0 +1,34 @@
+insert into users values(null, "Miguel", "password");
+insert into users values(null, "Caldas", "password");
+insert into users values(null, "Rui", "password");
+insert into users values(null, "David", "password");
+insert into users values(null, "Diana", "password");
+insert into users values(null, "Magano", "password");
+insert into users values(null, "Pedro", "password");
+
+insert into categoria values(null, "Mercearia");
+insert into categoria values(null, "Bebidas");
+insert into categoria values(null, "Frescos");
+insert into categoria values(null, "Laticínios");
+insert into categoria values(null, "Congelados");
+insert into categoria values(null, "Bebé");
+insert into categoria values(null, "Higiene");
+insert into categoria values(null, "Limpeza");
+insert into categoria values(null, "Casa");
+insert into categoria values(null, "Animais");
+insert into categoria values(null, "Lazer");
+
+insert into produtos values(null, 1, "Pêssego em Calda", "Globo", "emb. 820 gr", "2,79 /un", "11,16 /kg", "img/small/1.jpeg", "img/large/1.jpeg");
+insert into produtos values(null, 1, "Pudim de Caramelo", "Continente", "emb. 90 gr", "0,34 /un", "3,78 /kg", "img/small/2.jpeg", "img/large/2.jpeg");
+insert into produtos values(null, 1, "Pudim de Caramelo", "Continente", "emb. 90 gr", "0,34 /un", "3,78 /kg", "img/small/2.jpeg", "img/large/2.jpeg");
+
+insert into listas values(null, "Hachathon Sonae", 1);
+insert into listas values(null, "Aniversário", 3);
+
+insert into listasUsers values(1, 1);
+insert into listasUsers values(1, 2);
+insert into listasUsers values(1, 4);
+insert into listasUsers values(2, 1);
+
+
+
